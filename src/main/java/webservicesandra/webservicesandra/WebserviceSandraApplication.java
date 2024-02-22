edcbf0cca9676847f0f3275dc3da44a67fc43391
Sandra1887 <93sanjep@molndal.gafe.se>
2024-02-22 11:05:28 +0100

    init commit

---
+++ src/main/java/webservicesandra/webservicesandra/WebserviceSandraApplication.java
@@ -0,0 +1,13 @@
+package webservicesandra.webservicesandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceSandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webservicesandra/webservicesandra/WebserviceSandraApplicationTests.java
@@ -0,0 +1,13 @@
+package webservicesandra.webservicesandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceSandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
