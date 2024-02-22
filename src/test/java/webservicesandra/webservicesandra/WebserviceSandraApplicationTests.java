afa56c10b9c1598478c4264800611eac2b225532
Sandra1887 <93sanjep@molndal.gafe.se>
2024-02-22 11:35:43 +0100

    added tests

--- src/test/java/webservicesandra/webservicesandra/WebserviceSandraApplicationTests.java
+++ src/test/java/webservicesandra/webservicesandra/WebserviceSandraApplicationTests.java
@@ -3,11 +3,24 @@ package webservicesandra.webservicesandra;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class WebserviceSandraApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void TestingMain() {
+		WebserviceSandraApplication main1 = new WebserviceSandraApplication();
+		WebserviceSandraApplication main2 = new WebserviceSandraApplication();
+
+		assertEquals(main1.addition(), main2.addition());
+	}
+
+	@Test
+	public void TestingAdd() {
+		WebserviceSandraApplication main = new WebserviceSandraApplication();
+
+		assertEquals(main.addition(), 10);
 	}
 
 }
